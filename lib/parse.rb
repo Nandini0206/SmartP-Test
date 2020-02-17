a9eb2ae0760f669d73d1377c101551f34849a43f
Nandini Patel <nandini_patel96@hotmail.co.uk>
2020-02-17 10:30:48 +0000

    added method for reading file to array and then splitting ip and webpage - test now passing

--- lib/parse.rb
+++ lib/parse.rb
@@ -1,4 +1,16 @@
 class Parse
 
+  attr_reader :contents
+
+  def initialize
+    @contents = []
+  end
+
+  def parser(file)
+    File.foreach(file) do |line|
+      @contents << line
+    end
+    @contents = @contents.map { |i| i.split(' ') }
+  end
 
 end
