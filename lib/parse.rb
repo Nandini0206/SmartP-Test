672f6c89ba775e31496fb7962701e3a5fddf35c7
Nandini Patel <nandini_patel96@hotmail.co.uk>
2020-02-17 10:54:27 +0000

    fixed rubocop errors

--- lib/parse.rb
+++ lib/parse.rb
@@ -1,5 +1,4 @@
 class Parse
-
   attr_reader :contents, :page_views, :uniq_views
 
   def initialize
@@ -20,7 +19,7 @@ class Parse
   def most_viewed
     views = Hash.new(0)
     @page_views = @contents.map { |line| line[0] }
-    @page_views = @page_views.each  { |url| views[url] += 1 }
+    @page_views = @page_views.each { |url| views[url] += 1 }
     @page_views = views.sort_by { |_k, v| v }.reverse
   end
 
--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -1,9 +1,7 @@
-require "parse"
+require 'parse'
 
-
- describe Parse do
-
-   describe '#parser' do
+describe Parse do
+  describe '#parser' do
     it 'parses contents of file when file exists' do
       expect(subject.parser('webserver.log')).to include(['/contact', '184.123.665.067'])
     end
@@ -13,18 +11,17 @@ require "parse"
     end
   end
 
-   describe "#most_viewed" do
-     it "shows the most viewed pages" do
-       subject.parser("webserver.log")
-       expect(subject.most_viewed).to eq([["/about/2", 90], ["/contact", 89], ["/index", 82], ["/about", 81], ["/help_page/1", 80], ["/home", 78]])
-     end
-   end
-
-   describe '#unique_views' do
-    it 'shows a list of the most unique page views in descending order' do
+  describe '#most_viewed' do
+    it 'shows the most viewed pages' do
       subject.parser('webserver.log')
-      expect(subject.unique_views).to eq([["/index", 23], ["/home", 23], ["/contact", 23], ["/help_page/1", 23], ["/about/2", 22], ["/about", 21]])
+      expect(subject.most_viewed).to eq([['/about/2', 90], ['/contact', 89], ['/index', 82], ['/about', 81], ['/help_page/1', 80], ['/home', 78]])
     end
   end
 
- end
+  describe '#unique_views' do
+    it 'shows a list of the most unique page views in descending order' do
+      subject.parser('webserver.log')
+      expect(subject.unique_views).to eq([['/index', 23], ['/home', 23], ['/contact', 23], ['/help_page/1', 23], ['/about/2', 22], ['/about', 21]])
+    end
+  end
+end
