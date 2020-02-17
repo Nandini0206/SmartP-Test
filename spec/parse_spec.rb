890c195b4d61278e449fbbd50749d90616a5be24
Nandini Patel <nandini_patel96@hotmail.co.uk>
2020-02-17 10:29:15 +0000

    ammended test for parsing file

--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -5,7 +5,7 @@ require "parse"
 
    describe "#parser" do
      it "parses contents of file" do
-       expect(subject.parser("webserver.log")).to include(["/help_page/", "1 126.318.035.038", "/contact", "184.123.665.067"])
+       expect(subject.parser("webserver.log")).to include(['/contact', '184.123.665.067'])
      end
    end
 
