7960369e243f7fe0fd62ab3612428a115f2e06a3
Nandini Patel <nandini_patel96@hotmail.co.uk>
2020-02-17 10:37:39 +0000

    added unique_views test with mock results

--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -16,4 +16,11 @@ require "parse"
      end
    end
 
+   describe '#unique_views' do
+    it 'shows a list of the most unique page views in descending order' do
+      subject.parser('webserver.log')
+      expect(subject.unique_views).to eq([["/about/2", 90], ["/contact", 89], ["/index", 82]])
+    end
+  end
+
  end
