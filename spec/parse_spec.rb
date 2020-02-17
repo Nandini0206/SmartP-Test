43f8f16e4b939881ef31a28e5503697125d046fd
Nandini Patel <nandini_patel96@hotmail.co.uk>
2020-02-17 10:32:32 +0000

    added test for most_viewed pages

--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -9,4 +9,11 @@ require "parse"
      end
    end
 
+   describe "#most_viewed" do
+     it "shows the most viewed pages" do
+       subject.parser("webserver.log")
+       expect(subject.most_viewed).to eq([["/about/2", 20], ["/contact", 80], ["/help_page/1", 50]])
+     end
+   end
+
  end
