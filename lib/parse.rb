5814e8227140e3103e50588c15268a9eebc002f0
Nandini Patel <nandini_patel96@hotmail.co.uk>
2020-02-17 10:36:24 +0000

    replaced most_viewed test with correct results and added method for most_viewed

--- lib/parse.rb
+++ lib/parse.rb
@@ -1,9 +1,10 @@
 class Parse
 
-  attr_reader :contents
+  attr_reader :contents, :page_views
 
   def initialize
     @contents = []
+    @page_views = []
   end
 
   def parser(file)
@@ -13,4 +14,10 @@ class Parse
     @contents = @contents.map { |i| i.split(' ') }
   end
 
+  def most_viewed
+    views = Hash.new(0)
+    @page_views = @contents.map { |line| line[0] }
+    @page_views = @page_views.each  { |url| views[url] += 1 }
+    @page_views = views.sort_by { |_k, v| v }.reverse
+  end
 end
--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -5,14 +5,14 @@ require "parse"
 
    describe "#parser" do
      it "parses contents of file" do
-       expect(subject.parser("webserver.log")).to include(['/contact', '184.123.665.067'])
+       expect(subject.parser("webserver.log")).to include(["/contact", "184.123.665.067"])
      end
    end
 
    describe "#most_viewed" do
      it "shows the most viewed pages" do
        subject.parser("webserver.log")
-       expect(subject.most_viewed).to eq([["/about/2", 20], ["/contact", 80], ["/help_page/1", 50]])
+       expect(subject.most_viewed).to eq([["/about/2", 90], ["/contact", 89], ["/index", 82], ["/about", 81], ["/help_page/1", 80], ["/home", 78]])
      end
    end
 
